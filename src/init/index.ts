4fa8dc709e599539fd4ddd95c39a9a3231169e6b
Saad Ahmad <saad.ahmad@servian.com>
2023-08-18 00:22:09 +1000

    version update

--- src/init/index.ts
+++ src/init/index.ts
@@ -5,189 +5,189 @@ import { execSync } from 'child_process';
 import { writeOrAppendToFile, writeToFileIfNotExists } from '../common';
 
 export default function init() {
-  prompt.start();
+    prompt.start();
 
-  const schema = {
-    properties: {
-      projectType: {
-        description:
-          'Do you want to use JavaScript or TypeScript? <default: TypeScript>',
-        default: 'TypeScript',
-        pattern: /JavaScript|TypeScript/,
-        message: 'Invalid option. Choose between JavaScript or TypeScript',
-        required: true,
-      },
-      sourceDir: {
-        description: 'Where will the code reside? <default: src>',
-        default: 'src',
-        message: 'Invalid directory path',
-        required: true,
-        conform: checkOrCreateDir,
-      },
-      functionName: {
-        description:
-          'What is the name of your function? <default: my-segment-destination-function>',
-        default: 'my-segment-destination-function',
-        required: true,
-      },
-      functionType: {
-        description:
-          'What is the type of your segment function (only Destination available at the moment)? <default: Destination>',
-        pattern: /Destination/,
-        message:
-          'Invalid option. Only Destination option available at the moment',
-        default: 'Destination',
-        required: true,
-      },
-      settingsFile: {
-        description:
-          'Name of the settings file (this defines the setting/ config of the the segment function)? <default: segment.config.json>',
-        default: 'segment.config.json',
-        required: true,
-      },
-      transpiledCodeDir: {
-        description: 'Where will the transpiled code reside? <default: .dist>',
-        default: '.dist',
-        message: 'Invalid directory path',
-        required: true,
-        conform: checkOrCreateDir,
-      },
-      buildDir: {
-        description: 'Where will the final build reside? <default: .build>',
-        default: '.build',
-        message: 'Invalid directory path',
-        required: true,
-        conform: checkOrCreateDir,
-      },
-    },
-  };
-
-  prompt.get(schema, (err, result) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    const config = {
-      name: result.functionName,
-      description: 'A custom segment function',
-      type: result.functionType,
-      transpiler: result.projectType,
-      directories: {
-        source: result.sourceDir,
-        transpiled: result.transpiledCodeDir,
-        build: result.buildDir,
-      },
-      segment: {
-        settings: [],
-      },
+    const schema = {
+        properties: {
+            projectType: {
+                description:
+                    'Do you want to use JavaScript or TypeScript? <default: TypeScript>',
+                default: 'TypeScript',
+                pattern: /JavaScript|TypeScript/,
+                message: 'Invalid option. Choose between JavaScript or TypeScript',
+                required: true,
+            },
+            sourceDir: {
+                description: 'Where will the code reside? <default: src>',
+                default: 'src',
+                message: 'Invalid directory path',
+                required: true,
+                conform: checkOrCreateDir,
+            },
+            functionName: {
+                description:
+                    'What is the name of your function? <default: my-segment-destination-function>',
+                default: 'my-segment-destination-function',
+                required: true,
+            },
+            functionType: {
+                description:
+                    'What is the type of your segment function (only Destination available at the moment)? <default: Destination>',
+                pattern: /Destination/,
+                message:
+                    'Invalid option. Only Destination option available at the moment',
+                default: 'Destination',
+                required: true,
+            },
+            settingsFile: {
+                description:
+                    'Name of the settings file (this defines the setting/ config of the the segment function)? <default: segment.config.json>',
+                default: 'segment.config.json',
+                required: true,
+            },
+            transpiledCodeDir: {
+                description: 'Where will the transpiled code reside? <default: .dist>',
+                default: '.dist',
+                message: 'Invalid directory path',
+                required: true,
+                conform: checkOrCreateDir,
+            },
+            buildDir: {
+                description: 'Where will the final build reside? <default: .build>',
+                default: '.build',
+                message: 'Invalid directory path',
+                required: true,
+                conform: checkOrCreateDir,
+            },
+        },
     };
 
-    writeToFileIfNotExists(
-      path.join(process.cwd(), result.settingsFile as string),
-      JSON.stringify(config, null, 2),
-    );
+    prompt.get(schema, (err, result) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
 
-    writeToFileIfNotExists(
-      path.join(process.cwd(), '.babelrc'),
-      JSON.stringify(
-        {
-          presets: ['@babel/preset-env', '@babel/preset-typescript'],
-          plugins: ['@babel/plugin-transform-modules-commonjs'],
-        },
-        null,
-        2,
-      ),
-    );
+        const config = {
+            name: result.functionName,
+            description: 'A custom segment function',
+            type: result.functionType,
+            transpiler: result.projectType,
+            directories: {
+                source: result.sourceDir,
+                transpiled: result.transpiledCodeDir,
+                build: result.buildDir,
+            },
+            segment: {
+                settings: [],
+            },
+        };
 
-    writeToFileIfNotExists(
-      path.join(process.cwd(), 'tsconfig.json'),
-      JSON.stringify(
-        {
-          compilerOptions: {
-            target: 'ESNext',
-            module: 'ESNext',
-            strict: true,
-            esModuleInterop: true,
-            skipLibCheck: true,
-            forceConsistentCasingInFileNames: true,
-            isolatedModules: true,
-            outDir: `./${result.transpiledCodeDir}`,
-          },
-          include: [`./${result.sourceDir}/**/*.ts`],
-        },
-        null,
-        2,
-      ),
-    );
-
-    writeToFileIfNotExists(
-      path.join(process.cwd(), 'package.json'),
-      JSON.stringify(
-        {
-          name: config.name,
-          version: '1.0.0',
-          description: config.description,
-          main: `./${config.directories.build}/index.ts`,
-          scripts: {
-            build: `npx tsc && npx babel ./${config.directories.transpiled} --out-file ./${config.directories.build}/index.js`,
-            dev: `npx ts-node ./${config.directories.source}/index.ts`,
-          },
-          keywords: [],
-          author: 'segment-cdp-developer',
-          license: 'MIT',
-          devDependencies: {},
-        },
-        null,
-        2,
-      ),
-    );
-
-    // For .gitignore
-    writeOrAppendToFile(
-      path.join(process.cwd(), '.gitignore'),
-      `${config.directories.build}\n${config.directories.transpiled}\nnode_modules`,
-    );
-
-    console.log('Configuration saved successfully!');
-
-    try {
-      if (result.projectType === 'TypeScript') {
-        execSync(
-          'npm install typescript ts-node segment-cdp-functions --save-dev',
-          {
-            stdio: 'inherit',
-          },
+        writeToFileIfNotExists(
+            path.join(process.cwd(), result.settingsFile as string),
+            JSON.stringify(config, null, 2),
         );
-      }
-      execSync(
-        'npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/preset-typescript @babel/plugin-transform-modules-commonjs',
-        { stdio: 'inherit' },
-      );
-      execSync('git init', {
-        stdio: 'inherit',
-      });
-    } catch (error) {
-      console.error('Failed to install packages:', (error as Error).message);
-      return;
-    }
 
-    console.log('Project initialised successfully!');
-  });
+        writeToFileIfNotExists(
+            path.join(process.cwd(), '.babelrc'),
+            JSON.stringify(
+                {
+                    presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                    plugins: ['@babel/plugin-transform-modules-commonjs'],
+                },
+                null,
+                2,
+            ),
+        );
+
+        writeToFileIfNotExists(
+            path.join(process.cwd(), 'tsconfig.json'),
+            JSON.stringify(
+                {
+                    compilerOptions: {
+                        target: 'ESNext',
+                        module: 'ESNext',
+                        strict: true,
+                        esModuleInterop: true,
+                        skipLibCheck: true,
+                        forceConsistentCasingInFileNames: true,
+                        isolatedModules: true,
+                        outDir: `./${result.transpiledCodeDir}`,
+                    },
+                    include: [`./${result.sourceDir}/**/*.ts`],
+                },
+                null,
+                2,
+            ),
+        );
+
+        writeToFileIfNotExists(
+            path.join(process.cwd(), 'package.json'),
+            JSON.stringify(
+                {
+                    name: config.name,
+                    version: '1.0.0',
+                    description: config.description,
+                    main: `./${config.directories.build}/index.ts`,
+                    scripts: {
+                        build: config.type === "TypeScript" ? `npx tsc && npx babel ./${config.directories.transpiled} --out-dir ./${config.directories.build}` : `npx babel ./${config.directories.source} --out-dir ./${config.directories.build}`,
+                        dev: `npx ts-node ./${config.directories.source}/index.ts`,
+                    },
+                    keywords: [],
+                    author: 'segment-cdp-developer',
+                    license: 'MIT',
+                    devDependencies: {},
+                },
+                null,
+                2,
+            ),
+        );
+
+        // For .gitignore
+        writeOrAppendToFile(
+            path.join(process.cwd(), '.gitignore'),
+            `${config.directories.build}\n${config.directories.transpiled}\nnode_modules`,
+        );
+
+        console.log('Configuration saved successfully!');
+
+        try {
+            if (result.projectType === 'TypeScript') {
+                execSync(
+                    'npm install typescript ts-node segment-cdp-functions --save-dev',
+                    {
+                        stdio: 'inherit',
+                    },
+                );
+            }
+            execSync(
+                'npm install --save-dev @babel/core @babel/cli @babel/preset-env @babel/preset-typescript @babel/plugin-transform-modules-commonjs',
+                { stdio: 'inherit' },
+            );
+            execSync('git init', {
+                stdio: 'inherit',
+            });
+        } catch (error) {
+            console.error('Failed to install packages:', (error as Error).message);
+            return;
+        }
+
+        console.log('Project initialised successfully!');
+    });
 }
 
 function checkOrCreateDir(value: string) {
-  if (!fs.existsSync(value)) {
-    try {
-      fs.mkdirSync(value, { recursive: true });
-      console.log(`Directory did not exist, so I created it for you.`);
-      return true;
-    } catch (error) {
-      console.error(
-        `Failed to create directory. Please check permissions and try again.`,
-      );
-      return false;
+    if (!fs.existsSync(value)) {
+        try {
+            fs.mkdirSync(value, { recursive: true });
+            console.log(`Directory did not exist, so I created it for you.`);
+            return true;
+        } catch (error) {
+            console.error(
+                `Failed to create directory. Please check permissions and try again.`,
+            );
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
