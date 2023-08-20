1420685b0199dff1c6142644b856d3a225a499ac
Saad Ahmad <saad.ahmad@servian.com>
2023-08-20 23:10:23 +1000

    update

--- src/build/index.ts
+++ src/build/index.ts
@@ -5,21 +5,45 @@ import path from 'path';
 import log from "../common/log";
 
 export function build() {
-    const configFilePath = path.join(process.cwd(), "segment.config.json")
-    const config = readJson<ConfigType>(configFilePath)
+    try {
+        const configFilePath = path.join(process.cwd(), "segment.config.json");
 
-    const bundleJSPath = path.join(process.cwd(), config.directories.build, 'index.js')
-    const bundleJS = readFileContentSync(bundleJSPath)
+        if (!fs.existsSync(configFilePath)) {
+            throw new Error("Configuration file segment.config.json not found!")
+        }
 
-    fs.writeFileSync(
-        path.join(process.cwd(), config.directories.build, 'to_deploy.json'),
-        JSON.stringify({
-            settings: config.segment.settings,
-            code: bundleJS,
-            displayName: config.name,
-            logoUrl: "",
-            resourceType: config.type === "Destination" ? "DESTINATION" : "SOURCE",
-            description: config.description
-        }, null, 2)
-    )
+        const config = readJson<ConfigType>(configFilePath);
+
+        const outputDir = path.join(process.cwd(), config.directories.build);
+
+        if (!fs.existsSync(outputDir)) {
+            throw new Error(`Build directory ${outputDir} not found!`);
+        }
+
+        const bundleJSPath = path.join(outputDir, 'index.js');
+
+        if (!fs.existsSync(bundleJSPath)) {
+            throw new Error("Bundle JS file index.js not found in build directory!");
+        }
+
+        const bundleJS = readFileContentSync(bundleJSPath).split("\n").splice(1, 0, "var exports = {}").join("\n");
+
+        const outputFilePath = path.join(outputDir, 'to_deploy.json');
+        fs.writeFileSync(
+            outputFilePath,
+            JSON.stringify({
+                settings: config.segment.settings,
+                code: bundleJS,
+                displayName: config.name,
+                logoUrl: "",
+                resourceType: config.type === "Destination" ? "DESTINATION" : "SOURCE",
+                description: config.description
+            }, null, 2)
+        );
+
+        log.log("Build process completed successfully!");
+
+    } catch (error) {
+        throw error
+    }
 }
